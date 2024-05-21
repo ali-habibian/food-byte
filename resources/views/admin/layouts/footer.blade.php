5674b48fe5e3543846e8f0cf7135f4c700efd163
Ali Habibian <ali.habibian04@gmail.com>
2024-05-22 01:27:14 +0330

    Implemented Admin Dashboard and Mastering

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2024
+        <div class="bullet"></div>
+        Design By <a href="#">Ali Habibian</a>
+    </div>
+    <div class="footer-right">
+
+    </div>
+</footer>
