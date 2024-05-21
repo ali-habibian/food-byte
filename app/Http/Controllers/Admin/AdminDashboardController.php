487b56ffacd1d4cd389725f79be18b3cf5e123fb
Ali Habibian <ali.habibian04@gmail.com>
2024-05-21 21:33:48 +0330

    Added some documentation

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
+/**
+ * This controller handles requests related to the admin dashboard.
+ */
 class AdminDashboardController extends Controller
 {
+    /**
+     * Display the admin dashboard.
+     *
+     * @return View
+     */
     public function index(): View
     {
         return view('admin.dashboard.index');
--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -6,12 +6,20 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * This middleware checks if the authenticated user has the specified role.
+ */
 class RoleMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     * @param Request $request The incoming request
+     * @param Closure $next The next middleware to execute
+     * @param string $role The required role for this route
+     * @return Response
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException If user doesn't have the required role
      */
     public function handle(Request $request, Closure $next, $role): Response
     {
