eca73d8e6de33b5e98c32d330fa715b53942135e
Ali Habibian <ali.habibian04@gmail.com>
2024-05-23 00:09:06 +0330

    Frontend Setup Mastering Frontend Template

---
+++ resources/views/frontend/home/index.blade.php
@@ -0,0 +1,96 @@
+@extends('frontend.home.layouts.master')
+
+@section('content')
+    <!--=============================
+    BANNER START
+==============================-->
+    @include('frontend.home.components.banner')
+    <!--=============================
+        BANNER END
+    ==============================-->
+
+
+    <!--=============================
+        WHY CHOOSE START
+    ==============================-->
+    @include('frontend.home.components.why-choose')
+    <!--=============================
+        WHY CHOOSE END
+    ==============================-->
+
+
+    <!--=============================
+        OFFER ITEM START
+    ==============================-->
+    @include('frontend.home.components.offer-item')
+
+    <!-- CART POPUP START -->
+    @include('frontend.home.components.cart-popup')
+    <!-- CART POPUP END -->
+    <!--=============================
+        OFFER ITEM END
+    ==============================-->
+
+
+    <!--=============================
+        MENU ITEM START
+    ==============================-->
+    @include('frontend.home.components.menu-item')
+    <!--=============================
+        MENU ITEM END
+    ==============================-->
+
+
+    <!--=============================
+        AD SLIDER START
+    ==============================-->
+    @include('frontend.home.components.ad-slider')
+    <!--=============================
+        AD SLIDER END
+    ==============================-->
+
+
+    <!--=============================
+        TEAM START
+    ==============================-->
+    @include('frontend.home.components.team')
+    <!--=============================
+        TEAM END
+    ==============================-->
+
+
+    <!--=============================
+        DOWNLOAD APP START
+    ==============================-->
+    @include('frontend.home.components.app-download')
+    <!--=============================
+        DOWNLOAD APP END
+    ==============================-->
+
+
+    <!--=============================
+       TESTIMONIAL  START
+    ==============================-->
+    @include('frontend.home.components.testimonial')
+    <!--=============================
+        TESTIMONIAL END
+    ==============================-->
+
+
+    <!--=============================
+        COUNTER START
+    ==============================-->
+    @include('frontend.home.components.counter')
+    <!--=============================
+        COUNTER END
+    ==============================-->
+
+
+    <!--=============================
+        BLOG 2 START
+    ==============================-->
+    @include('frontend.home.components.blog')
+    <!--=============================
+        BLOG 2 END
+    ==============================-->
+@endsection
