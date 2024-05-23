26d753a095509123889dc4e074d35282fc219544
Ali Habibian <ali.habibian04@gmail.com>
2024-05-24 00:33:55 +0330

    Implemented Frontend Registration Page Mastering

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,4 @@
+{{--
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
@@ -50,3 +51,97 @@
         </div>
     </form>
 </x-guest-layout>
+--}}
+
+
+@extends('frontend.home.layouts.master')
+
+@section('content')
+    <!--=============================
+        BREADCRUMB START
+    ==============================-->
+    {{--<section class="fp__breadcrumb" style="background: url(images/counter_bg.jpg);">
+        <div class="fp__breadcrumb_overlay">
+            <div class="container">
+                <div class="fp__breadcrumb_text">
+                    <h1>sign up</h1>
+                    <ul>
+                        <li><a href="index.html">home</a></li>
+                        <li><a href="#">sign up</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>--}}
+    <!--=============================
+        BREADCRUMB END
+    ==============================-->
+
+
+    <!--=========================
+        SIGN UP START
+    ==========================-->
+    <section class="fp__signup" style="background: url({{asset('frontend/images/login_bg.jpg')}});">
+        <div class="fp__signup_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
+            <div class=" container">
+                <div class="row wow fadeInUp" data-wow-duration="1s">
+                    <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
+                        <div class="fp__login_area mt-4">
+                            <h2 class="mb-4">sign up</h2>
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+
+                                <div class="row">
+                                    <!-- Name -->
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <input id="name" name="name" type="text" placeholder="Name"
+                                                   value="{{old('name')}}" required autofocus autocomplete="name"/>
+                                        </div>
+                                    </div>
+
+                                    <!-- Email Address -->
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <input id="email" name="email" type="email" placeholder="Email"
+                                                   value="{{old('email')}}" required autocomplete="username"/>
+                                        </div>
+                                    </div>
+
+                                    <!-- Password -->
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <input type="password" id="password" name="password" placeholder="Password"
+                                                   required
+                                                   autocomplete="new-password">
+                                        </div>
+                                    </div>
+
+                                    <!-- Confirm Password -->
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <input type="password" id="password_confirmation"
+                                                   name="password_confirmation"
+                                                   required autocomplete="new-password" placeholder="Confirm Password">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <button type="submit" class="common_btn">Register</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
+                            <p class="create_account">Already registered? <a href="{{route('login')}}">Login</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=========================
+        SIGN UP END
+    ==========================-->
+@endsection
