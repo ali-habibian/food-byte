f8500a1913fdfff32be8da524996c1fc9af0ce69
Ali Habibian <ali.habibian04@gmail.com>
2024-05-26 01:14:11 +0330

    Added Documentation

--- app/Traits/FileUploadTrait.php
+++ app/Traits/FileUploadTrait.php
@@ -6,6 +6,14 @@ use Illuminate\Http\Request;
 
 trait FileUploadTrait
 {
+    /**
+     * Uploads an image from the given request and saves it to the specified path.
+     *
+     * @param Request $request The HTTP request object.
+     * @param string $inputName The name of the input field containing the image.
+     * @param string $path The path to save the image (default: '/uploads').
+     * @return string|null The path to the uploaded image, or null if no file was uploaded.
+     */
     function uploadImage(Request $request, $inputName, $path = '/uploads')
     {
         if ($request->hasFile($inputName)) {
